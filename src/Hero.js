189de523d3ac668deb0f2386c4c55f9b6b36e215
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-05-13 16:11:58 +0100

    removed useState

--- src/Hero.js
+++ src/Hero.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
