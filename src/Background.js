4084e7edfb60839190a00cb4209441cf3b8f72bf
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-05-13 16:08:22 +0100

    committing change in background.js

--- src/Background.js
+++ src/Background.js
@@ -12,11 +12,11 @@ const Background = ({ playStatus, heroCount }) => {
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className="background fade-in" alt="car image" />;
+    return <img src={image1} className="background fade-in" />;
   } else if (heroCount === 1) {
-    return <img src={image2} className="background fade-in" alt="car image" />;
+    return <img src={image2} className="background fade-in" />;
   } else if (heroCount === 2) {
-    return <img src={image3} className="background fade-in" alt="car image" />;
+    return <img src={image3} className="background fade-in" />;
   }
 
   return null;
