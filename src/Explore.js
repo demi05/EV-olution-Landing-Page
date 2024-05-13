82bb678cc484faf0fecb435ad4619227055b2379
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-05-13 16:19:32 +0100

    added alt prop

--- src/Explore.js
+++ src/Explore.js
@@ -4,6 +4,14 @@ const Explore = () => {
   return (
     <div className="explore">
       <h2>Welcome to EV-olution!</h2>
+      <div className="explore-content">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
